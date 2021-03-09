159df912f6207606a7a9539e4e386ef4f8340b64
medhabibhnini <60883388+medhabibhnini@users.noreply.github.com>
2021-03-09 14:56:09 +0100

    update db connection

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb+srv://habibhnini:habib123456@cluster0.yx34s.mongodb.net/databasetest?retryWrites=true&w=majority",
+  mongoURI: "mongodb+srv://improve:improve123@cluster0.i9hik.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
   secretOrKey: "secret"
 };
